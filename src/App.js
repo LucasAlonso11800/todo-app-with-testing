466a4e2c4c209d947a979b4413c7e0970ed2cb0b
Lucas <lucasalonso11800@gmail.com>
2021-11-20 15:03:46 -0300

    Basic testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+    const [items, setItems] = useState([]);
+    const [text, setText] = useState("");
 
-export default App;
+    const handleChange = (e) => setText(e.target.value);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!text.length) return;
+
+        const newItem = {
+            text,
+            id: Date.now(),
+            completed: false
+        };
+
+        setText("");
+        setItems(items.concat(newItem));
+    };
+
+    const handleRemove = (item) => setItems(items.filter(i => i.id !== item.id));
+    const handleClickOnComplete = (item) => {
+        if (item.completed) setItems([{ ...item, completed: false }].concat(items.filter(i => i.id !== item.id)));
+        if (!item.completed) setItems(items.filter(i => i.id !== item.id).concat({ ...item, completed: true }))
+    };
+
+    return (
+        <div >
+            <h1 className="title">TODOS</h1>
+
+            <ul className="todo-list">
+                {items.map((item) => (
+                    <li className={`todo ${item.completed ? 'completed' : 'toComplete'}`} key={item.id}>{item.text}
+                        <span onClick={() => handleClickOnComplete(item)}>{item.completed ? 'O' : 'X'}</span>
+                        <span onClick={() => handleRemove(item)}><button>Remove</button></span>
+                    </li>
+                ))}
+            </ul>
+
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="new-todo">What needs to be done?</label>
+                <br />
+                <input id="new-todo" value={text} onChange={handleChange} />
+                <button disabled={text === ''}>Add #{items.length + 1}</button>
+            </form>
+        </div>
+    );
+};
+
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('App', () => {
+    test('Adds item to the list, marks it as complete and then removes it', () => {
+        const { getByText, getByLabelText } = render(<App />);
+
+        const input = getByLabelText('What needs to be done?');
+        const button = getByText('Add #1');
+        expect(button).toBeDisabled();
+
+        userEvent.type(input, 'First todo');
+        expect(button).toBeEnabled();
+        userEvent.click(button);
+
+        getByText('First todo');
+        getByText('Add #2');
+
+        const markAsCompleteButton = getByText('X');
+        userEvent.click(markAsCompleteButton);
+        getByText('O');
+
+        const removeButton = getByText('Remove');
+        userEvent.click(removeButton);
+        getByText('Add #1');
+    });
 });
